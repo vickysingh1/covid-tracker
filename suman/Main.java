b5780fd2404eaf56ab0f69ad2c0dda4e71a291db
divya640 <divyakumari2872000@gmail.com>
2022-06-19 19:23:04 +0530

    h

---
+++ suman/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+public static void main(String[] args) {
+    int n = 4;
+    
+    if(n%2==0)
+    {
+        System.out.println("no is even");
+    }
+    else
+    {
+        System.out.println("no is odd"); 
+    }
+}
+}
